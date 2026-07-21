9d0bebe766499c6649d45d63392780db28ae2ec6
Elena Keda <elena_keda_92@mail.ru>
2026-07-21 23:51:34 +0300

    added post and get controllers

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,30 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: 1,
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/books.jpg",
+        creator: { name: "Test user" },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
+
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: { name: "Test user" },
+      createdAt: new Date(),
+    },
   });
 };
